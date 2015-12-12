23918592e90a600b9bc601654257466d70a088fa
notr3d <lxmrrr@gmail.com>
2015-12-12 18:00:59 +0300

    оживление числовых полей раздела "попутчики"

--- js/script.js
+++ js/script.js
@@ -20,39 +20,53 @@ var durationButtonMinus = c("vacation__duration-minus"),
     durationInput = c("vacation__duration-count"),
     departureInput = c("vacation__start"),
     arrivalInput = c("vacation__end"),
-    duration = parseInt(durationInput.value),
-    departureDate = departureInput.value.toString().split("-"),
+    duration = parseInt(durationInput.value);
+    /*departureDate = departureInput.value.toString().split("-"),
     departureYear = +departureDate[0],
     departureMonth = +departureDate[1] - 1,
     departureDay = +departureDate[2],
     arrivalDay = departureDay + duration,
-    arrivalDate = new Date(departureYear, departureMonth, arrivalDay);
-//    arrivalDate = arrivalInput.value.toString().split("-"),
-//    nyear = arrivalDate[0],
-//    nmonth = arrivalDate[1] - 1,
-//    nday = arrivalDate[2],
-//    arrival = new Date(nyear, nmonth, nday);
-//    
-//alert(arrival);
+    arrivalDate = new Date(departureYear, departureMonth, arrivalDay);*/
+
 
 durationButtonMinus.addEventListener("click", function(){
     if(duration < 1)return;
     duration--;
     durationInput.value = duration + " дней";
-})
+});
 durationButtonPlus.addEventListener("click", function(){
     duration++;
     durationInput.value = duration + " дней";
-})
+});
 durationInput.addEventListener("change", function(){
     if(isNaN(parseInt(durationInput.value))){
         alert("Неверное значение продолжительности путешествия");
         durationInput.value = 10 + " дней";
     }else{
         duration = parseInt(durationInput.value);
-        arrivalInput.value = arrivalDate;
     }
-})
+});
 
+//кнопки для количества попутчиков
+var companionsButtonMinus = c("companions__minus"),
+    companionsButtonPlus = c("companions__plus"),
+    companionsInput = c("companions__count"),
+    companionsCount = parseInt(companionsInput.value);
 
-
+companionsButtonMinus.addEventListener("click", function(){
+    if(companionsCount < 1)return;
+    companionsCount--;
+    companionsInput.value = companionsCount + " чел";
+});
+companionsButtonPlus.addEventListener("click", function(){
+    companionsCount++;
+    companionsInput.value = companionsCount + " чел";
+});
+companionsInput.addEventListener("change", function(){
+    if(isNaN(parseInt(companionsInput.value))){
+        alert("Неверное значение количества попутчиков");
+        companionsInput.value = 2 + "  чел";
+    }else{
+        companionsCount = parseInt(companionsInput.value);
+    }
+});
