326169f2b8f69830788f979fee33db5a8ce044be
notr3d <lxmrrr@gmail.com>
2015-12-14 14:18:34 +0300

    Реализации загрузки картинок

--- js/xhr.js
+++ js/xhr.js
@@ -18,8 +18,40 @@
         
         var data = new FormData(form);
         
+        queue.forEach(function(element){
+            data.append("images", element.file);    
+        });
         request(data, function(response){
             alert(response);
         });
     });
+    
+    
+    //загрузка фотографий
+    var queue = [];
+    form.querySelector(".photo__upload-file").addEventListener("change", function(){
+        var files = this.files;
+        for(var i = 0; i < files.length; i++){
+            preview(files[i]);    
+        }
+    });
+    function preview(file){
+        var area = c("photo__preview"),
+            template = '<figure class="photo__item"><img src="{{image}}" alt="{{name}}"><figcaption>{{name}}</figcaption><button>&times;</button></figure>';
+        
+        if(file.type.match(/image.*/)){
+            var reader = new FileReader();
+            reader.addEventListener("load", function(event){
+                var html = template.replace("{{image}}", event.target.result);
+                html = html.replace("{{name}}", file.name);
+                area.innerHTML = area.innerHTML + html;
+                /*var img = document.createElement("img");
+                img.src = event.target.result;
+                img.alt = file.name;
+                area.appendChild(img);*/
+                //queue.push({file: file, img: img});
+            });
+            reader.readAsDataURL(file);
+        }
+    };
 })();
