e906dda2c1df82e473fc4818aa8b602a34c23b2b
notr3d <lxmrrr@gmail.com>
2015-12-14 16:38:45 +0300

    Подключение mustashe; доработка загрузки фотографий

--- js/xhr.js
+++ js/xhr.js
@@ -1,8 +1,10 @@
 //отправка формы с помощью ajax
 (function(){
+    /*if(!("FormData it window") || !(FileReader in window)){
+        return;
+    }*/
     function request(data, f){
         var xhr = new XMLHttpRequest();
-        
         xhr.open("post", "https://echo.htmlacademy.ru/adaptive?");
         xhr.addEventListener("readystatechange", function(){
             if (xhr.readyState == 4){
@@ -26,8 +28,8 @@
         });
     });
     
-    
     //загрузка фотографий
+    var area = c("photo__preview");
     var queue = [];
     form.querySelector(".photo__upload-file").addEventListener("change", function(){
         var files = this.files;
@@ -36,22 +38,35 @@
         }
     });
     function preview(file){
-        var area = c("photo__preview"),
-            template = '<figure class="photo__item"><img src="{{image}}" alt="{{name}}"><figcaption>{{name}}</figcaption><button>&times;</button></figure>';
+        var template = c("image-template").innerHTML;
         
         if(file.type.match(/image.*/)){
             var reader = new FileReader();
             reader.addEventListener("load", function(event){
-                var html = template.replace("{{image}}", event.target.result);
-                html = html.replace("{{name}}", file.name);
-                area.innerHTML = area.innerHTML + html;
-                /*var img = document.createElement("img");
-                img.src = event.target.result;
-                img.alt = file.name;
-                area.appendChild(img);*/
-                //queue.push({file: file, img: img});
+                var html = Mustache.render(template, {
+                    "image": event.target.result,
+                    "name": file.name
+                });
+                var figure = document.createElement("figure");
+                figure.classList.add("photo__item");
+                figure.innerHTML = html;
+                area.appendChild(figure);
+                figure.querySelector(".photo__delete").addEventListener("click", function(event){
+                    removePreview(figure);
+                });
+                queue.push({
+                    "file": file,
+                    "figure": figure
+                })
             });
             reader.readAsDataURL(file);
         }
     };
+    function removePreview(figure){
+        queue = queue.filter(function(element){
+            return element.figure != figure;
+        });
+        var figure = figure.parentNode;
+        figure.parentNode.removeChild(figure);
+    }
 })();
