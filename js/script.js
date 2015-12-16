ee54b9df2842c6051b40967fd0defa2595c0518d
notr3d <lxmrrr@gmail.com>
2015-12-16 21:47:41 +0300

    Доработка верстки, реализация ввода даты с клавиатуры

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/*var firstSlide = document.getElementById("reviews__control-1");
+var secondSlide = document.getElementById("reviews__control-2");
+var thirdSlide = document.getElementById("reviews__control-3");
+var slider = c("reviews__slider");
+
+firstSlide.addEventListener("click", function(){
+    slider.classList.add("reviews__slider--first");
+    slider.classList.remove("reviews__slider--second");
+    slider.classList.remove("reviews__slider--third");
+});
+secondSlide.addEventListener("click", function(){
+    slider.classList.remove("reviews__slider--first");
+    slider.classList.add("reviews__slider--second");
+    slider.classList.remove("reviews__slider--third");
+});
+thirdSlide.addEventListener("click", function(){
+    slider.classList.remove("reviews__slider--first");
+    slider.classList.remove("reviews__slider--second");
+    slider.classList.add("reviews__slider--third");
+});*/
